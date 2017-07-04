f4544fad74ffcb485de09aa1521459fa65376e3c
sy_samojlenko <sy_samojlenko@bingo-boom.ru>
2017-07-04 13:21:13 +0300

    add postgres support to migration

--- migrations/m161005_124320_uploads.php
+++ migrations/m161005_124320_uploads.php
@@ -6,33 +6,64 @@ class m161005_124320_uploads extends Migration
 {
     public function up()
     {
-        $this->db->createCommand("CREATE TABLE `uploads` ( 
-            `id` BIGINT(20) NOT NULL AUTO_INCREMENT , 
-            `attribute` VARCHAR(255) NOT NULL ,
-            `model` VARCHAR(255) NOT NULL , 
-            `parent_id` BIGINT(20) NOT NULL , 
-            `mime_type` VARCHAR(255) NULL DEFAULT NULL , 
-            `size` BIGINT(20) NULL DEFAULT NULL , 
-            `original_name` VARCHAR(255) NULL DEFAULT NULL , 
-            `params` TEXT NULL DEFAULT NULL ,
-            `name` VARCHAR(255) NULL DEFAULT NULL , 
-            `hash` VARCHAR(40) NULL DEFAULT NULL , 
-            `extension` VARCHAR(10) NULL DEFAULT NULL , 
-            `type` TINYINT(3) NULL DEFAULT NULL , 
-            `created_at` TIMESTAMP NULL DEFAULT NULL , 
-            `updated_at` TIMESTAMP NULL DEFAULT NULL , 
-            PRIMARY KEY (`id`), 
-            INDEX `linked` (`model`, `parent_id`, `attribute`),
-            INDEX `hash` (`hash`),
-            INDEX `name` (`name`)
-        ) ENGINE = InnoDB;")->execute();
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('uploads', [
+            'id' => $this->primaryKey(),
+            'attribute' => $this->string()->notNull(),
+            'model' => $this->string()->notNull(),
+            'parent_id' => $this->integer()->notNull(),
+            'mime_type' => $this->string(),
+            'size' => $this->integer(),
+            'original_name' => $this->string(),
+            'params' => $this->text(),
+            'name' => $this->string(),
+            'hash' => $this->string(40),
+            'extension' => $this->string(),
+            'type' => $this->smallInteger(3),
+            'created_at' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP')->notNull(),
+            'updated_at' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP')->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex(
+            'uploads_linked',
+            'uploads',
+            ['model', 'parent_id', 'attribute']
+        );
+        $this->createIndex(
+            'uploads_hash',
+            'uploads',
+            'hash'
+        );
+        $this->createIndex(
+            'uploads_name',
+            'uploads',
+            'name'
+        );
     }
 
     public function down()
     {
-        echo "m161005_124320_uploads cannot be reverted.\n";
+        $this->dropIndex(
+            'uploads_linked',
+            'uploads'
+        );
 
-        return false;
+        $this->dropIndex(
+            'uploads_hash',
+            'uploads'
+        );
+
+        $this->dropIndex(
+            'uploads_name',
+            'uploads'
+        );
+
+        $this->dropTable('uploads');
     }
 
     /*
